91cd3107e305a2a17ad58e06d99e80cb8ba656cb
wuchubuzai <2009longyu@163.com>
2024-07-21 12:49:53 +0800

    提交初版项目文件

---
+++ openai-code-review-test/src/test/java/cn/aijavapro/middleware/test/ApiTest.java
@@ -0,0 +1,24 @@
+package cn.aijavapro.middleware.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author Wuchubuzai
+ * @version 1.0
+ * 2024年07月21日 11:21
+ */
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiTest {
+    
+    @Test
+    public void test() {
+        System.out.println(Integer.parseInt("aaaa"));
+    }
+    
+}
