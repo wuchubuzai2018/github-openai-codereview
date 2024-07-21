1036349c95c99ae580af5bc2703f5755d0744831
wuchubuzai <2009longyu@163.com>
2024-07-21 19:44:18 +0800

    提交一个openai的workflow文件

--- openai-code-review-sdk/src/main/java/cn/aijavapro/middleware/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/cn/aijavapro/middleware/sdk/OpenAiCodeReview.java
@@ -8,7 +8,7 @@ package cn.aijavapro.middleware.sdk;
 public class OpenAiCodeReview {
     
     public static void main(String[] args) {
-        System.out.println("测试执行6");
+        System.out.println("测试执行7");
     }
     
     public void test(){
