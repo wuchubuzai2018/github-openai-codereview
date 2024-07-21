1cf1dc583d8d0ad9efa1675420e6555163f58e2d
wuchubuzai <2009longyu@163.com>
2024-07-21 12:49:53 +0800

    提交初版项目文件

---
+++ openai-code-review-sdk/src/main/java/cn/aijavapro/middleware/sdk/OpenAiCodeReview.java
@@ -0,0 +1,14 @@
+package cn.aijavapro.middleware.sdk;
+
+/**
+ * @author Wuchubuzai
+ * @version 1.0
+ * 2024年07月21日 11:26
+ */
+public class OpenAiCodeReview {
+    
+    public static void main(String[] args) {
+        System.out.println("测试执行");
+    }
+    
+}
---
+++ openai-code-review-test/src/main/java/cn/aijavapro/middleware/Application.java
@@ -0,0 +1,5 @@
+package cn.aijavapro.middleware;
+
+public class Application {
+
+}
