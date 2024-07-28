34a7a250e52df66fc0fa8a7638b86bc02e36d90d
wuchubuzai <2009longyu@163.com>
2024-07-28 11:21:27 +0800

    feat:增加变更代码读取

--- openai-code-review-sdk/src/main/java/cn/aijavapro/middleware/sdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/cn/aijavapro/middleware/sdk/OpenAiCodeReview.java
@@ -1,5 +1,12 @@
 package cn.aijavapro.middleware.sdk;
 
+import org.apache.commons.io.IOUtils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * @author Wuchubuzai
  * @version 1.0
@@ -7,10 +14,24 @@ package cn.aijavapro.middleware.sdk;
  */
 public class OpenAiCodeReview {
     
-    public static void main(String[] args) {
-        System.out.println("测试执行8");
+    public static void main(String[] args) throws Exception {
+        System.out.println("开始执行CodeReview");
+        
+        // 1.执行本地命令进行代码检出的操作
+        String code =checkGitChangeCode();
     }
     
-  
+    
+    private static String checkGitChangeCode() throws Exception {
+        ProcessBuilder processBuilder = new ProcessBuilder("git", "diff", "HEAD-1", "HEAD");
+        processBuilder.directory(new File("."));
+        Process process = processBuilder.start();
+        String diffCode = IOUtils.toString(process.getInputStream());
+        int result = process.waitFor();
+        System.out.println("执行退出:" + result);
+        System.out.println("变更代码:" + diffCode);
+        return diffCode;
+    }
+    
     
 }
